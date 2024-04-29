421461e1a438c50617adf832009eddbffe8620b4
elena.liu <elena.liu@baifu-tech.net>
2024-04-29 10:29:49 +0800

    add cSettings

--- Package.swift
+++ Package.swift
@@ -16,7 +16,11 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "NewTestLibrary",
-            path: "Sources"
+            path: "Sources",
+//            publicHeadersPath: "Sources",
+            cSettings: [
+                .headerSearchPath("."),
+            ]
         ),
         .testTarget(
             name: "NewTestLibraryTests",
